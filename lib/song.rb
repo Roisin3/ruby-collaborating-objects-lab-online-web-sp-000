a1d98890a16838cfda78bb1168a7aacf6c368414
Michael Lynch <michael@michaelrlynch.com>
2019-09-24 05:04:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,7 +20,7 @@ class Song
     artist.add_song(self)
   end
 
-  def new_by_filename(filename)
+  def self.new_by_filename(filename)
     artist, song_name = filename.split(" - ")
     new_song = self.new(song)
     new_song.artist_name = artist_name
