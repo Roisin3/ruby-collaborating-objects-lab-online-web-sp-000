5b848a21092956ab5895daf621471b2981d73a0b
Michael Lynch <michael@michaelrlynch.com>
2019-09-24 23:09:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,7 +4,7 @@ class Artist
 
   def initialize(name)
     @name = name
-    @@all << self
+    save
   end
 
   def self.all
@@ -17,6 +17,22 @@ class Artist
 
   def songs
     Song.all.select {|song| song.artist == self}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name}
+  end
+
+  def save
+    @@all << self
+  end
+  
+  def print_songs
+    songs.each {|song| puts song.name}
   end
 
 end
